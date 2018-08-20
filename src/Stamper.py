44a5631f4ace9a3a88c48b2e8b236a756d752b45
Adam Chesak <achesak@yahoo.com>
2018-08-20 11:46:04 -0400

    Simplify some code

--- src/Stamper.py
+++ src/Stamper.py
@@ -1,33 +1,37 @@
 import os
 import shutil
+import sys
+
+
+def get_user_input(message):
+    # Support Python 2
+    if sys.version_info >= (3, 0):
+        return input(message)
+    else:
+        return raw_input(message)
 
 
 def create_project_at(project_path):
     folder_names = ["docs", "src", "test"]
     file_names = ["setup.py", "LICENSE.txt", "requirements.txt", "Makefile"]
-    project_name = input("What is the name of the project? ")
-    print(project_path + "\\" + project_name)
+    project_name = get_user_input("What is the name of the project? ")
+    print(os.path.join(project_path, project_name))
     create_folder(project_path, project_name, True)
-    fin_proj_path = project_path + "\\" + project_name + "\\"
-    i = 0
-    while i < 3:
-        create_folder(fin_proj_path, folder_names[i])
-        i += 1
-    i = 0
-    while i < 4:
-        create_file(fin_proj_path, file_names[i])
-        i += 1
+    fin_proj_path = os.path.join(project_path, project_name)
+    for folder_name in folder_names:
+        create_folder(fin_proj_path, folder_name)
+    for file_name in file_names:
+        create_file(fin_proj_path, file_name)
 
 
 def create_folder(dir_path, folder_name, is_project=False):
-    des_dir = dir_path + "\\" + folder_name + "\\"
+    des_dir = os.path.join(dir_path, folder_name)
     try:
         if is_project:
             if not os.path.exists(des_dir):
                 os.makedirs(des_dir)
             else:
-                wtr = wants_to_replace()
-                if wtr:
+                if wants_to_replace():
                     print("Replaced It!!")
                     shutil.rmtree(des_dir)
                     os.makedirs(des_dir)
@@ -45,23 +49,16 @@ def create_folder(dir_path, folder_name, is_project=False):
 
 def create_file(file_path, file_name):
     fin_file_path = os.path.join(file_path, file_name)
-    test_to_write = None
-    if file_name == "setup.py":
-        test_to_write = "Package and distribution management."
-    elif file_name == "sample.py":
-        test_to_write = "The code of interest."
-    elif file_name == "test_sample.py":
-        test_to_write = "Package integration and unit tests."
-    elif file_name == "requirements.txt":
-        test_to_write = "Development dependencies."
-    elif file_name == "LICENSE.txt":
-        test_to_write = "Lawyering up."
-    elif file_name == "Makefile.txt":
-        test_to_write = "Generic management tasks."
-    file_obj = open(fin_file_path, "w+")
-    if test_to_write is not None:
-        file_obj.write(test_to_write)
-    file_obj.close()
+    file_contents = {
+        "setup.py": "Package and distribution management.",
+        "sample.py": "The code of interest.",
+        "test_sample.py": "Package integration and unit tests.",
+        "requirements.txt": "Development dependencies.",
+        "LICENSE.txt": "Lawyering up.",
+        "Makefile": "Generic management tasks.",
+    }
+    with open(fin_file_path, "w+") as proj_file:
+        proj_file.write(file_contents[file_name])
 
 
 def check_folder_name(folder_name):
@@ -75,13 +72,13 @@ def check_folder_name(folder_name):
 
 
 def wants_to_replace():
-    ret_val = input("There is already a project with the same name.\n"
-                   "Do you want to replace it?(y/n) ")
-    while ret_val.lower() != "y" and ret_val.lower() != "n":
-        print("Please Try Again")
-        ret_val = input("Do you want to replace it?(y/n) ")
-    if ret_val.lower() == "y":
-        ret_val = True
-    else:
-        ret_val = False
-    return ret_val
+    ret_val = get_user_input(
+        "There is already a project with the same name.\n"
+        "Do you want to replace it? (y/n) "
+    )
+    while ret_val.lower() not in ["y", "n"]:
+        ret_val = get_user_input(
+            "Please Try Again\n"
+            "Do you want to replace it? (y/n) "
+        )
+    return ret_val.lower() == "y"
